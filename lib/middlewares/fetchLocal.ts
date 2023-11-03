fd3c43ea30b7ac90fae4559f71c94e64ec444b88
David Bonnet <david@bonnet.cc>
2023-11-03 12:40:32 +0100

    Fix `fetchLocal` type

--- lib/middlewares/fetchLocal.ts
+++ lib/middlewares/fetchLocal.ts
@@ -6,9 +6,24 @@ import type { CustomFieldMap, Handler, Query } from "../types";
  *
  * @returns Promise that resolves to the response.
  */
+export function fetchLocal<T extends object>(): Handler<
+  Query<T, {}> & { type: T[]; customFields: never },
+  any,
+  never,
+  never
+>;
 export function fetchLocal<
   T extends object,
   C extends CustomFieldMap<T>,
->(): Handler<Query<T, C> & { type: T[] }, any, never, never> {
-  return async (query, _) => queryItemList<T, C>(query);
+>(): Handler<Query<T, C> & { type: T[]; customFields: C }, any, never, never>;
+export function fetchLocal<
+  T extends object,
+  C extends CustomFieldMap<T>,
+>(): Handler<
+  Query<T, C> & { type: T[]; customFields: C | never },
+  any,
+  never,
+  never
+> {
+  return async (query, _) => queryItemList(query);
 }
