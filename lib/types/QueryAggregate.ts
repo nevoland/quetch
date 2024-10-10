02c8ef2441f500a625162c708a05439131f1d05d
David Bonnet <david@bonnet.cc>
2024-10-10 08:53:58 +0200

    Add list options to `QueryAggregate`

--- lib/types/QueryAggregate.ts
+++ lib/types/QueryAggregate.ts
@@ -1,6 +1,7 @@
 import type { AggregateFunction } from "./AggregateFunction";
 import type { Context } from "./Context";
 import type { Filter } from "./Filter";
+import type { Order } from "./Order";
 import type { Parameters } from "./Parameters";
 import type { QuerySettings } from "./QuerySettings";
 
@@ -14,7 +15,22 @@ export type QueryAggregate<T extends object> = {
    */
   context?: Context<T>;
   aggregator: AggregateFunction<T>;
+  /**
+   * Filter that picks the items.
+   */
   filter?: Filter<T>;
+  /**
+   * Order by which the items should be sorted.
+   */
+  order?: Order<T>[];
+  /**
+   * Offset of the first matching item.
+   */
+  offset?: number;
+  /**
+   * Upper bound of the number of items to return.
+   */
+  limit?: number;
   /**
    * Query parameters.
    */
