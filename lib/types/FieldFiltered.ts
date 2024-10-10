3d87b4964bba694ae026eeb3768e9759c5f74862
David Bonnet <david@bonnet.cc>
2024-10-10 08:54:38 +0200

    Ensure `FieldFiltered` uses all properties

--- lib/types/FieldFiltered.ts
+++ lib/types/FieldFiltered.ts
@@ -1,7 +1,6 @@
-import type { Join } from "./Join";
 import type { KeyFiltered } from "./KeyFiltered";
 import type { PathFiltered } from "./PathFiltered";
 
 export type FieldFiltered<T extends object, P> =
-  | KeyFiltered<T, P>
-  | PathFiltered<T, P>;
+  | KeyFiltered<Required<T>, P>
+  | PathFiltered<Required<T>, P>;
