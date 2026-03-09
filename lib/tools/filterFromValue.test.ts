a14993f4200aa0613624e2b1291e810bc292d79e
David Bonnet <david@bonnet.cc>
2026-03-10 00:18:51 +0100

    Update `filterFromValue` tests

--- lib/tools/filterFromValue.test.ts
+++ lib/tools/filterFromValue.test.ts
@@ -6,64 +6,34 @@ import { filterFromValue } from "./filterFromValue.js";
 
 test("returns filter from context", () => {
   expect(filterFromValue({ a: 1 })).toEqual({
-    operator: "all",
-    value: [
-      {
-        field: "a",
-        operator: "equal",
-        value: 1,
-      },
-    ],
+    field: "a",
+    operator: "equal",
+    value: 1,
   });
   expect(filterFromValue({ a: { b: 1 } })).toEqual({
-    operator: "all",
-    value: [
-      {
-        field: ["a", "b"],
-        operator: "equal",
-        value: 1,
-      },
-    ],
+    field: ["a", "b"],
+    operator: "equal",
+    value: 1,
   });
   expect(filterFromValue({ a: { b: undefined } })).toEqual({
-    operator: "all",
-    value: [
-      {
-        field: ["a", "b"],
-        operator: "equal",
-        value: undefined,
-      },
-    ],
+    field: ["a", "b"],
+    operator: "equal",
+    value: undefined,
   });
   expect(filterFromValue({ a: { b: null } })).toEqual({
-    operator: "all",
-    value: [
-      {
-        field: ["a", "b"],
-        operator: "equal",
-        value: null,
-      },
-    ],
+    field: ["a", "b"],
+    operator: "equal",
+    value: null,
   });
   expect(filterFromValue({ [SELF]: 3 })).toEqual({
-    operator: "all",
-    value: [
-      {
-        field: SELF,
-        operator: "equal",
-        value: 3,
-      },
-    ],
+    field: SELF,
+    operator: "equal",
+    value: 3,
   });
   expect(filterFromValue({ a: null })).toEqual({
-    operator: "all",
-    value: [
-      {
-        field: "a",
-        operator: "equal",
-        value: null,
-      },
-    ],
+    field: "a",
+    operator: "equal",
+    value: null,
   });
   expect(filterFromValue({ a: { b: 1 }, c: 2 })).toEqual({
     operator: "all",
