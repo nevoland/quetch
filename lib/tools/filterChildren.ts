78e51f8989e150bad7c9687ad573f6d1f074683e
David Bonnet <david@bonnet.cc>
2026-03-10 11:41:41 +0100

    Fix `filterChildren`

--- lib/tools/filterChildren.ts
+++ lib/tools/filterChildren.ts
@@ -1,29 +1,33 @@
-import type { FieldFiltered, IntrinsicFilter } from "../types.ts";
+import type { FieldFiltered, FilterStringMatch } from "../types.js";
 
 import { escapeRegex } from "./escapeRegex.js";
 
+/**
+ * Generates a filter that matches items whose path field indicates they are children of a given parent path, with an optional depth range.
+ *
+ * @param parentPath - The path of the parent item.
+ * @param pathFieldKey - The key of the field that contains the path information.
+ * @param minDepth - The minimum depth of the child items relative to the parent.
+ * @param maxDepth - The maximum depth of the child items relative to the parent.
+ * @param pathSeparator - The character used to separate path segments.
+ * @returns A filter that can be used to match child items.
+ */
 export function filterChildren<T>(
   parentPath: string,
   pathFieldKey: FieldFiltered<T, string>,
   minDepth = 0,
   maxDepth = Infinity,
   pathSeparator = "/",
-): IntrinsicFilter<T> {
+): FilterStringMatch<T> {
+  const escapedSeparator = escapeRegex(pathSeparator);
+  const segment = `${escapedSeparator}[^${escapedSeparator}]+`;
+  const quantifier =
+    minDepth === 0 && maxDepth === 1
+      ? ""
+      : `{${minDepth + 1},${maxDepth === Infinity ? "" : maxDepth + 1}}`;
   return {
     field: pathFieldKey,
     operator: "match",
-    value: childrenRegex(parentPath, minDepth, maxDepth, pathSeparator),
+    value: `^${escapeRegex(parentPath)}(?:${segment})${quantifier}$`,
   };
-}
-
-function childrenRegex(
-  parentPath: string,
-  minDepth = 0,
-  maxDepth = Infinity,
-  pathSeparator = "/",
-): string {
-  const escapedSeparator = escapeRegex(pathSeparator);
-  return `^${escapeRegex(parentPath)}${
-    parentPath === "" ? "" : escapedSeparator
-  }${minDepth === 0 && maxDepth === Infinity ? "" : `(?:[^${escapedSeparator}]{1,}){${minDepth + 1},${maxDepth === Infinity ? "" : maxDepth}}$`}`;
 }
