8f20cae296ae66b506143ff6ab9396fccbb0ac6b
David Bonnet <david@bonnet.cc>
2026-03-14 11:36:31 +0100

    Document `Context` type

--- lib/types/Context.ts
+++ lib/types/Context.ts
@@ -2,6 +2,14 @@ import type { SELF } from "../constants/SELF.js";
 
 import type { Primitive } from "./Primitive.js";
 
+/**
+ * Describes the entity so that it can be identified or selected in a filter.
+ *
+ * The context of a single item (`query.multiple` is `false`) is typically its identifier.
+ * The context of a list of items (`query.multiple` is `true`) typically consists of common properties that reference the parent entity.
+ *
+ * It can be a partial object of the type `T` or a primitive value wrapped in an object with the `SELF` key.
+ */
 export type Context<T> = [0] extends [1 & T]
   ? any
   : T extends Primitive
